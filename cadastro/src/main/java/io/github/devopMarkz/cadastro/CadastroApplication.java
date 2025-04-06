54f0a371d8f653d73d061457f3aa04f821c1e10e
devopMarkz <marcosdev2002@gmail.com>
2025-04-06 00:05:50 -0300

    First commit

---
+++ cadastro/src/main/java/io/github/devopMarkz/cadastro/CadastroApplication.java
@@ -0,0 +1,13 @@
+package io.github.devopMarkz.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroApplication.class, args);
+	}
+
+}
