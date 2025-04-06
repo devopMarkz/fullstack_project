4aee63109855fc6f4bdda99c15949a66202e6846
devopMarkz <marcosdev2002@gmail.com>
2025-04-06 00:30:04 -0300

    Criando controller para Usuario com metodo POST

--- backend/src/main/java/io/github/devopMarkz/backend/controllers/UsuarioController.java
+++ backend/src/main/java/io/github/devopMarkz/backend/controllers/UsuarioController.java
@@ -1,4 +1,31 @@
 package io.github.devopMarkz.backend.controllers;
 
+import io.github.devopMarkz.backend.model.Usuario;
+import io.github.devopMarkz.backend.repositories.UsuarioRepository;
+import io.github.devopMarkz.backend.services.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin("*")
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> insertUsuario(@RequestBody Usuario usuario) {
+        var novoUsuario = usuarioService.insertUsuario(usuario);
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(novoUsuario.getId())
+                        .toUri()
+        ).build();
+    }
 }
