793aa1c739188ea6dc02e837f8f88a3192c78101
devopMarkz <marcosdev2002@gmail.com>
2025-04-06 00:29:51 -0300

    Criando service para Usuario

--- backend/src/main/java/io/github/devopMarkz/backend/services/UsuarioService.java
+++ backend/src/main/java/io/github/devopMarkz/backend/services/UsuarioService.java
@@ -1,4 +1,22 @@
 package io.github.devopMarkz.backend.services;
 
+import io.github.devopMarkz.backend.model.Usuario;
+import io.github.devopMarkz.backend.repositories.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository ;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Transactional
+    public Usuario insertUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
 }
